550044bd59860061c88f3383533f5415ab8ad8bd
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2024-05-23 16:05:55 -0500

    fix: :wheelchair: Add label to button with no discernible text

--- src/pages/loan/components/MarkPaymentAsPaidDialog.tsx
+++ src/pages/loan/components/MarkPaymentAsPaidDialog.tsx
@@ -24,7 +24,10 @@ export default function MarkPaymentAsPaidDialog({
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
-        <button className='grid aspect-square w-12 place-content-center rounded-full border-2 border-neutral-200 bg-neutral-200  text-primary transition-colors group-hover:border-primary group-hover:bg-white'>
+        <button
+          aria-label='Marcar como pagado'
+          className='grid aspect-square w-12 place-content-center rounded-full border-2 border-neutral-200 bg-neutral-200  text-primary transition-colors group-hover:border-primary group-hover:bg-white'
+        >
           <PencilIcon
             size={20}
             className='opacity-0 transition-opacity group-hover:opacity-100'
